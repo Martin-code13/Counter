148364fc3278b40063baae3dae3c69277ecc2c82
Martin-code13 <mstelo234@gmail.com>
2023-01-10 13:51:47 +0000

    Mi primer commit

---
+++ src/js/component/Secondscounter.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+
+
+
+
+const Secondscounter = (props) => {
+	return (
+		
+	<div className="bg-dark text-danger p-3 d-flex">
+		<h1>Contador: {props.numero6} {props.numero5} {props.numero4} {props.numero3} {props.numero2} {props.numero1}</h1>
+		
+	</div>
+
+	
+        
+);
+ };
+
+ export default Secondscounter;
+
